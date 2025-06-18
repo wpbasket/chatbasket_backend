430c23c87294b905656d0d7df7e42f0866b532a7
Nitish Raj <hellowpbasket@gmail.com>
2025-06-18 14:09:09 +0530

    first commit

---
+++ chatbasket/app/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+	}))
+
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello!")
+}
