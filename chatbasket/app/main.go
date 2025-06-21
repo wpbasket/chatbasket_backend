508dd4c3dd7df04e15c4a6c4b672149cc8c39de4
Nitish Raj <hellowpbasket@gmail.com>
2025-06-21 12:42:52 +0530

    Add Dockerfile, .dockerignore, .gitignore, samconfig.toml, and template.yaml; update main.go to change endpoint and response

--- chatbasket/app/main.go
+++ chatbasket/app/main.go
@@ -14,11 +14,11 @@ func main() {
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
 	}))
-
-	e.GET("/", hello)
+	
+	e.GET("/hello", hello)
 	e.Logger.Fatal(e.Start(":8000"))
 }
 
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello!")
+	return c.String(http.StatusOK, "Hello")
 }
